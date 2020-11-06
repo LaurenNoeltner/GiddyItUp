74916d07eb1609ee738bf772b3f30524bdf8a70e
Kesha Hall <k_renee10@yahoo.com>
2020-11-06 13:52:32 -0500

    added components

---
+++ client/src/components/CardBody.js/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CardBody(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default CardBody;
---
+++ client/src/components/CardHeading.js/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CardHeading(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default CardHeading;
