a200243ca440ec0d122e4fb53127f8ef7d5b51e5
Lauren Noeltner <laurennoeltner@gmail.com>
2020-11-06 13:42:14 -0500

    navbar working

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,38 @@
-const { default: Navbar } = require("./components/Navbar");
+import Header from "./components/Header";
+// import Home from "./pages/Home";
+import Bounty from "./components/Bounty";
+import Saloon from "./components/Saloon";
+import React, { Component } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  // Link,
+  // Redirect,
+  // useLocation,
+} from "react-router-dom";
+// import Footer from "./components/Footer";
+// import axios from "axios";
 
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <header className="App-header">
-          <h1>Whaddup React Rulz</h1>
-          <Navbar />
-          <Switch>
-            <Route exact path="/Saloon" component={Saloon} />
-            <Route exact path="/Bounty" component={Bounty} />
-          </Switch>
-        </header>
-      </div>
-    </Router>
-  );
+const { default: Navbar } = require("./components/Header");
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <h1>Whaddup React Rulz</h1>
+            <Header />
+            <Switch>
+              <Route exact path="/Saloon" component={Saloon} />
+              <Route exact path="/Bounty" component={Bounty} />
+            </Switch>
+          </header>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
+import Navbar from 'react-bootstrap/Navbar'
+import { NavLink } from "react-router-dom";
 
-class Navbar extends Component {
+class Header extends Component {
     render() {
         return (
             <div>
-                <Container>
+                <div>
                     <Navbar expand="lg" variant="light" bg="light">
                         <Navbar.Brand href="#">GiddyItUp</Navbar.Brand>
                         <div className="headBtn"><NavLink to="/Saloon"   activeClassName="selected">Saloon</NavLink></div>
                         <div className="headBtn"><NavLink to="/Bounty" activeClassName="selected">Bounty Board</NavLink></div> 
                     </Navbar>
-                </Container>
+                </div>
             </div>
         );
     }
 }
 
-export default Navbar;
+export default Header;
