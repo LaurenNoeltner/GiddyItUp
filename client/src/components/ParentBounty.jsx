3101adc9a8623910d91c4e454b1e6818036e5bba
Lauren Noeltner <laurennoeltner@gmail.com>
2020-11-09 11:22:52 -0500

    merging main and working_on_components

--- client/src/components/ParentBounty.jsx
+++ client/src/components/ParentBounty.jsx
@@ -5,22 +5,94 @@ import { NavLink } from "react-router-dom";
 class ParentBounty extends Component {
     render() {
         return (
-            <div>
-                <div className="row">
-                <div className="col-md-3"></div>
-                <div className="col-md-3"></div>
-                <div className="col-md-3">
-                    <div className="row">
-                        <NavLink to="/ParentBounty" activeClassName="selected">Sheriff's Board</NavLink>
+            <>
+            <div className="row" >
+                <div className="col-md-4"></div>
+                <div id="boardTitle"className="col-md-3">Bounty Board</div>
+                <div className="col-md-4"></div>
+            </div>
+            <div className="row">
+                <div className="col-md-2"></div>
+                <div className="bountyContainer col-md-8">
+                    <div className="row" id="row1">
+                        <div className="taskContainer col-md-3">
+                            Reward: 45 silver | Walk Dogs
+                        </div>
+                        <div className="col-md-1"></div>
+                        <div className="taskContainer col-md-3">
+                            2
+                        </div>
+                        <div className="col-md-1"></div>
+                        <div className="taskContainer col-md-3">
+                            3
+                        </div>
                     </div>
-                    <div className="row">
-                        <NavLink to="/Bounty" activeClassName="selected">Bounty Board</NavLink>
+                    <div className="row" id="row2">
+                        <div className="taskContainer col-md-3">
+                           4
+                        </div>
+                        <div className="col-md-1"></div>
+                        <div className="taskContainer col-md-3">
+                           5 
+                        </div>
+                        <div className="col-md-1"></div>
+                        <div className="taskContainer col-md-3">
+                           6 
+                        </div>
+                    </div>
+                    <div className="row" id="row3">
+                        <div className="taskContainer col-md-3">
+                            7
+                        </div>
+                        <div className="col-md-1"></div>
+                        <div className="taskContainer col-md-3">
+                            8
+                        </div>
+                        <div className="col-md-1"></div>
+                        <div className="taskContainer col-md-3">
+                            9
+                        </div>
                     </div>
                 </div>
-                <div className="col-md-3"></div>
-                
+                <div className="col-md-2"></div>
             </div>
+            <hr />
+            <div className="row addTaskBox">
+                <div className="col-md-2"></div>
+                <div id="taskForm" className="col-md-7">
+                    <div className="row">
+                        <div className="col-md-3"></div>
+                        <div id= "formTitle" className="col-md-5">Add a Bounty!</div>
+                        <div className="col-md-3"></div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-3"></div>
+                        <div id="formBox" className="col-md-5">
+                            <label>Task Name</label>
+                            <input id="taskName" title="What is the task name?" required></input>
+                        </div>
+                        <div className="col-md-3"></div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-3"></div>
+                        <div id="formBox" className="col-md-5">
+                            <label>Description</label>
+                            <input id="description" title="Describe the task..." required></input>
+                        </div>
+                        <div className="col-md-3"></div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-3"></div>
+                        <div id="formBox" className="col-md-5">
+                            <label>Reward</label>
+                            <input id="rewardInput" placeholder="Silver Coins..." required></input>
+                        </div>
+                        <div className="col-md-3"></div>
+                    </div>
+                </div>
+                <div className="col-md-2"></div>
             </div>
+            </>
         );
     }
 }
