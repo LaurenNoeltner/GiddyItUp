4cc92a3bbcd5b1b910109ac016b75b92c76486b0
Lauren Noeltner <laurennoeltner@gmail.com>
2020-11-10 13:27:45 -0500

    got all delete buttons working correctlygit add .!

--- client/src/components/Bounty/Bounty.jsx
+++ client/src/components/Bounty/Bounty.jsx
@@ -1,5 +1,5 @@
-import React, { Component, useState, useEffect } from 'react';
-import Header from "../Header/Header";
+import React, { useState, useEffect } from 'react';
+
 // import ParentBounty from "../ParentBounty/ParentBounty";
 import "./Bounty.css";
 import API from "../utils/API"
@@ -26,6 +26,13 @@ function Bounty() {
                 })
                 .catch(err => console.log(err));
         };
+
+          // Deletes a book from the database with a given id, then reloads books from the db
+        function deleteTask(id) {
+            API.deleteTask(id)
+            .then(res => loadTasks())
+            .catch(err => console.log(err));
+        }
     
     
         //handles updating component state when user types into input field
@@ -72,6 +79,11 @@ function Bounty() {
                             <div>
                                 {task.location} | {task.description}
                             </div>
+                            <button onClick={() => deleteTask(task._id)}>
+                                <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-check" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.236.236 0 0 1 .02-.022z"/>
+                                </svg>
+                            </button>
                         </div>
                         </>
                     ))}    
--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -9,7 +9,7 @@ class Header extends Component {
             <div>
                 <div>
                     <Navbar expand="lg" variant="light" bg="light">
-                        <h1 id="navbarLogo" href="/">GiddyItUp</h1>
+                        <h1 id="navbarLogo" href="/Saloon">GiddyItUp</h1>
                         <div className="ml-3 headBtn"><NavLink id="headBtn" to="/Saloon"  activeClassName="selected">Saloon</NavLink></div>
                         <div className="ml-3 headBtn"><NavLink id="headBtn" to="/Bounty" activeClassName="selected">Bounty Board</NavLink></div> 
                         <div className="ml-3 headBtn"><NavLink id="headBtn" to="/ParentBounty" activeClassName="selected">Sheriff's Bounty Board</NavLink></div> 
