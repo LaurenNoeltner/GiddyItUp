a766fa38cbcfd00e2f9b67db9772695f4312dd7e
Lauren Noeltner <laurennoeltner@gmail.com>
2020-11-12 11:25:49 -0500

    Leif helped us get the points counter working

--- client/src/components/ParentBounty/ParentBounty.jsx
+++ client/src/components/ParentBounty/ParentBounty.jsx
@@ -3,19 +3,14 @@ import React, { useState, useEffect } from "react";
 import "./ParentBounty.css";
 import "../Bounty/Bounty.css";
 import API from "../utils/API";
-// import Counter from "../Counter/Counter";
+import Counter from "../Counter/Counter";
 
-function ParentBounty() {
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {
-    //         points: 0,
-    //         show: true,
-    //     };
-    // }
+function ParentBounty(props) {
+
 
   const [tasks, setTasks] = useState([]);
   const [formObject, setFormObject] = useState({});
+  const [points, setPoints] = useState(0);
   
 
   //load all tasks
@@ -53,6 +48,19 @@ function ParentBounty() {
         
     // }
 
+    function handleIncrement (task) {
+      // We always use the setState method to update a component's state
+      let newPoints = points;
+      newPoints = parseInt(newPoints) + parseInt(task.points);
+      console.log(newPoints);
+      setPoints(newPoints);
+      // console.log(props);
+      // props.state.points = tasks;
+      console.log(tasks, "this is supposed to say tasks");
+      deleteTask(task._id);
+      
+    };
+
 
 
 
@@ -82,60 +90,63 @@ function ParentBounty() {
 
   return (
     <>
-      <div className="row">
-        <div className="col-md-3"></div>
-        <div id="boardTitle" className="col-md-5">
-          Bounty Board
+    <div className="bounty-page">
+    <div className="container"></div>
+        <div className="row">
+            <div className="col-md-3"></div>
+            <div id="boardTitle" className="col-md-5">
+            Bounty Board
+            </div>
+            <div className="col-md-3">
+                <h3>Points: {points}</h3>
+            </div>
         </div>
-        <div className="col-md-3">
-            {/* <h3 id="Points">${props.points}</h3> */}
-        </div>
-      </div>
 
-      <div className="row bounty-container">
-        <div className="col-md-2"></div>
-        <div className="bountyContainer col-md-8">
-          <div className="row" id="row1">
-            {tasks.map((task) => (
-              <>
-                <div key={task.identifier} className="taskContainer col-md-3">
-                    <button className="delete-task" onClick={() => deleteTask(task._id)}>
-                      <svg
-                        width="1em"
-                        height="1em"
-                        viewBox="0 0 16 16"
-                        className="bi bi-x"
-                        fill="currentColor"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          fill-rule="evenodd"
-                          d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"
-                        />
-                      </svg>
-                    </button> 
-                    <div id="taskPoints">Bounty: ${task.points}</div>   
-                    <hr />
-                    <div id="taskName">  
-                        <strong>{task.task}</strong>  
-                    </div>
-                    <hr />
-                    <div id="placeText">
-                        <strong>Place:</strong> {task.location} 
-                    </div>
-                    <div id="notesText"><strong>Notes: </strong>{task.description}</div>
-                    {/* <button className="check-btn" onClick={props.handleIncrement()}>
-                        <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-check" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd" d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.236.236 0 0 1 .02-.022z"/>
+        <div className="row bounty-container">
+            <div className="col-md-2"></div>
+            <div className="bountyContainer col-md-8">
+            <div className="row" id="row1">
+                {tasks.map((task) => (
+                <>
+                    <div key={task.identifier} className="taskContainer col-md-3">
+                        <button className="delete-task" onClick={() => deleteTask(task._id)}>
+                        <svg
+                            width="1em"
+                            height="1em"
+                            viewBox="0 0 16 16"
+                            className="bi bi-x"
+                            fill="currentColor"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                            fill-rule="evenodd"
+                            d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"
+                            />
                         </svg>
-                    </button> */}
-                </div>
-              </>
-            ))}
-          </div>
+                        </button> 
+                        <div id="taskPoints">Bounty: ${task.points}</div>   
+                        <hr />
+                        <div id="taskName">  
+                            <strong>{task.task}</strong>  
+                        </div>
+                        <hr />
+                        <div id="placeText">
+                            <strong>Place:</strong> {task.location} 
+                        </div>
+                        <div id="notesText"><strong>Notes: </strong>{task.description}</div>
+                        <button className="check-btn" onClick={() => handleIncrement(task)}>
+                            <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-check" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.236.236 0 0 1 .02-.022z"/>
+                            </svg>
+                        </button>
+                    </div>
+                </>
+                ))}
+            </div>
+            </div>
+            <div className="col-md-2"></div>
         </div>
-        <div className="col-md-2"></div>
-      </div>
+        </div>
 
       <br />
       <form className="row addTaskBox">
