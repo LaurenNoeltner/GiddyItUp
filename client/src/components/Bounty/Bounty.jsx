8f00369c4562737de4f93f237a4778c8bbe1d9b3
Lauren Noeltner <laurennoeltner@gmail.com>
2020-11-11 19:29:22 -0500

    styled the bounty cards a bit more; got both bounty boards looking the same

--- client/src/components/Bounty/Bounty.jsx
+++ client/src/components/Bounty/Bounty.jsx
@@ -75,16 +75,16 @@ function Bounty() {
                             {tasks.map(task => (
                                 <>
                                     <div key={task.identifier} className="taskContainer col-md-3">
-                                    <div id="taskName">  
-                                        <strong>{task.task}</strong>  
-                                    </div>
-                                    <hr />
-                                    <div>${task.points}</div>
-                                    <hr />
-                                    <div id="placeText">
-                                        <em>Place:</em> {task.location} 
-                                    </div>
-                                    <div id="notesText"><em>Notes: </em>{task.description}</div>
+                                        <div id="taskPoints">Bounty: ${task.points}</div>   
+                                        <hr />
+                                        <div id="taskName">  
+                                            <strong>{task.task}</strong>  
+                                        </div>
+                                        <hr />
+                                        <div id="placeText">
+                                            <em>Place:</em> {task.location} 
+                                        </div>
+                                        <div id="notesText"><em>Notes: </em>{task.description}</div>
                                         {/* <button onClick={() => deleteTask(task._id)}>
                                 <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-check" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                     <path fill-rule="evenodd" d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.236.236 0 0 1 .02-.022z"/>
--- client/src/components/ParentBounty/ParentBounty.jsx
+++ client/src/components/ParentBounty/ParentBounty.jsx
@@ -95,12 +95,12 @@ function ParentBounty() {
                           d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"
                         />
                       </svg>
-                    </button>    
+                    </button> 
+                    <div id="taskPoints">Bounty: ${task.points}</div>   
+                    <hr />
                     <div id="taskName">  
                         <strong>{task.task}</strong>  
                     </div>
-                    <hr />
-                    <div>${task.points}</div>
                     <hr />
                     <div id="placeText">
                         <em>Place:</em> {task.location} 
